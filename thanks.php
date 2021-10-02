02b6b1d1fcc1742ad382ba7545740960d139e135
clr-89 <74655761+clr-89@users.noreply.github.com>
2021-10-02 23:07:09 +0200

    Create thanks.php

---
+++ thanks.php
@@ -0,0 +1,8 @@
+
+<p> Merci  <?php echo $_POST["user_name"];?> de nous avoir contacté à propos de  <?php echo $_POST["user_subject"];?>.<br>
+
+Un de nos conseiller vous contactera soit à l’adresse <?php echo $_POST["user_mail"];?> 
+  
+ou par téléphone au <?php echo $_POST["user_phoneNumber"];?> dans les plus brefs délais pour traiter votre demande : 
+ 
+</p>
